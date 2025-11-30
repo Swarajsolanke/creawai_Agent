7e75c40d26a4f559582b61d4bc700ff3028efc8f
Swarajsolanke <swarajsolanke02@gmail.com>
2025-11-30 19:48:31 +0530

    config file added

--- app.py
+++ app.py
@@ -1,18 +1,54 @@
-import pdfplumber
-pdf_path=r"C:\aptitude content\13.Permutations Combination.pdf"
-print(f"pdf_path:{pdf_path}")
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task
 
-with pdfplumber.open(pdf_path) as pdf:
-    text = []
-    for page in pdf.pages:
-        print(page)
-        text1 = page.extract_text()
-        print(text1)
-        if text1:
-            text.append(text1.strip())
-           
-        print(f"text:{text}")
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, DirectoryReadTool, FileWriterTool, FileReadTool
 
-print(text)
+from dotenv import load_dotenv
+load_dotenv()
 
+@CrewBase
+class BlogCrew():
+    """"Blog writing crew"""
 
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['research_agent'], # type: ignore[index]
+            tools=[SerperDevTool()],
+            verbose=True
+        )
+
+    @agent
+    def writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['writer_agent'], # type: ignore[index]
+            verbose=True
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'], # type: ignore[index]
+            agent = self.researcher()
+        )
+
+    @task
+    def blog_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['blog_task'], # type: ignore[index]
+            agent = self.writer()
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        return Crew(
+            agents=[self.researcher(), self.writer()],
+            tasks=[self.research_task(), self.blog_task()]
+        )
+
+if __name__ == "__main__":
+    blog_crew = BlogCrew()
+    blog_crew.crew().kickoff(inputs={"topic": "The future of electrical vehicles"})
